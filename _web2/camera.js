e57ef0cceb823d0f7e1135f19c5da919c2fc1b0e
Daniel Haehn <haehn@seas.harvard.edu>
2014-01-21 11:25:06 -0500

    Simplify code and move zoom logic to the camera.

--- _web2/camera.js
+++ _web2/camera.js
@@ -41,17 +41,48 @@ J.camera.prototype.reset = function() {
 
 ///
 
-J.camera.prototype.zoom_in = function() {
-  this._view[0]++;
-  this._view[4]++;
-};
+J.camera.prototype.zoom = function(x, y, delta) {
+  
+  var u_v = this._viewer.xy2uv(x,y);
 
-J.camera.prototype.zoom_out = function() {
-  this._view[0]--;
-  this._view[4]--;
+  // only do stuff if we are over the image data
+  if (u_v[0] == -1 || u_v[1] == -1) {
+    return;
+  }  
+
+  var wheel_sign = sign(delta/120);
+
+  var future_zoom_level = this._view[0] + wheel_sign;
+
+  // clamp zooming
+  if (future_zoom_level <= 0 || future_zoom_level > 20) return;
+
+  // check if we need to load tiles
+  if (future_zoom_level % 2 == 0) {
+    // yes
+    console.log('need loading', future_zoom_level);
+  }
+
+  var old_scale = this._view[0];
+
+  // perform zooming
+  this._view[0] += wheel_sign;
+  this._view[4] += wheel_sign;
+
+  var new_scale = this._view[0];
+
+  var u_new = u_v[0]/old_scale * new_scale;
+  var v_new = u_v[1]/old_scale * new_scale;
+
+  // translate to correct point
+  this._view[6] -= wheel_sign * Math.abs(u_v[0] - u_new);
+  this._view[7] -= wheel_sign * Math.abs(u_v[1] - v_new);
+
 };
 
 J.camera.prototype.pan = function(dx, dy) {
+
   this._view[6] += dx;
   this._view[7] += dy;
+
 };
--- _web2/interactor.js
+++ _web2/interactor.js
@@ -3,17 +3,12 @@ var J = J || {};
 J.interactor = function(viewer) {
 
   this._viewer = viewer;
-
-  this._scale = 1;
-  this._originx = 0;
-  this._originy = 0;  
+  this._camera = this._viewer._camera;
 
   this._left_down = false;
   this._right_down = false;
 
   this._last_mouse = [0,0];
-
-  this._last_offset = [0,0];
 
   this.init();
 
@@ -27,6 +22,7 @@ J.interactor.prototype.init = function() {
   // mouse down and up
   this._viewer._canvas.onmousedown = this.onmousedown.bind(this);
   this._viewer._canvas.onmouseup = this.onmouseup.bind(this);
+  // disable the context menu
   this._viewer._canvas.oncontextmenu = function() { return false; };
 
   // mouse wheel
@@ -43,28 +39,14 @@ J.interactor.prototype.onmousemove = function(e) {
 
   var u_v = this._viewer.xy2uv(x, y);
 
-  // jump out if we are not inside the real image data
-  // if (u_v[0] == -1 || u_v[1] == -1) {
-  //   return;
-  // }
-
   if (this._left_down) {
     
   } else if (this._right_down) {
     // pan
-    this._viewer._camera.pan(x-this._last_mouse[0], y-this._last_mouse[1]);    
+    this._camera.pan(x-this._last_mouse[0], y-this._last_mouse[1]);    
   }
 
-  //console.log(this._left_down, e.clientX, e.clientY, u_v[0], u_v[1], this._viewer._camera._view[0], this._viewer._camera._view[6]);
-
-  // console.log('XY', x, y);
-  // console.log('UV', u_v[0], u_v[1]);
-  // console.log('IJ', this._viewer.xy2ij(x, y));
-  // console.log('SCALE', this._viewer._camera._view[0])
-  // console.log('LASTOFFSET', this._last_offset);
-
   this._last_mouse = [x, y];
-  this._last_offset = [this._viewer._camera._view[6], this._viewer._camera._view[7]];
 
 };
 
@@ -74,6 +56,7 @@ J.interactor.prototype.onmousedown = function(e) {
     // left
     this._left_down = true;
   } else if (e.button == 2) {
+    // right
     this._right_down = true;
   }
 
@@ -93,37 +76,7 @@ if (e.button == 0) {
 
 J.interactor.prototype.onmousewheel = function(e) {
 
-  var canvas = this._viewer._canvas;
-  var context = this._viewer._context;
-
-  var x = e.clientX;
-  var y = e.clientY;
-  var u_v = this._viewer.xy2uv(x,y);
-  
-  if (u_v[0] == -1 || u_v[1] == -1) {
-    return;
-  }
-
-  var wheel_sign = sign(e.wheelDelta/120);
-
-  // clamp zooming
-  if (this._viewer._camera._view[0] + wheel_sign <= 0) return;
-  if (this._viewer._camera._view[0] + wheel_sign > 20) return;
-
-  var old_scale = this._viewer._camera._view[0];
-
-  // perform zooming
-  this._viewer._camera._view[0] += wheel_sign;
-  this._viewer._camera._view[4] += wheel_sign;
-
-  var new_scale = this._viewer._camera._view[0];
-
-  var u_new = u_v[0]/old_scale * new_scale;
-  var v_new = u_v[1]/old_scale * new_scale;
-
-  // translate to correct point
-  this._viewer._camera._view[6] -= wheel_sign * Math.abs(u_v[0] - u_new);
-  this._viewer._camera._view[7] -= wheel_sign * Math.abs(u_v[1] - v_new);
+  this._camera.zoom(e.clientX, e.clientY, e.wheelDelta);
 
 };
 
