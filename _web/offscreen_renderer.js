bc685ad087581f90d0bdc064821de6a2221b918d
Daniel Haehn <daniel.haehn@childrens.harvard.edu>
2014-02-20 09:08:08 -0500

    Playing around with the non-working merge functionality on the shaders.

--- _web/offscreen_renderer.js
+++ _web/offscreen_renderer.js
@@ -137,7 +137,7 @@ J.offscreen_renderer.prototype.draw = function(s, c, x, y) {
   var merge_table_values = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, merge_table_values);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, merge_table_length, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, this._controller._gl_merge_table_values);
-
+  
   // clamp to edge
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
