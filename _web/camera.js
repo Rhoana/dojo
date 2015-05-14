18dfd569ffae18491cbbf80344965032bdce57d6
haehn <haehn@seas.harvard.edu>
2015-05-14 15:01:11 -0400

    Fix bug where zooming out sometimes got stuck.

--- _web/camera.js
+++ _web/camera.js
@@ -104,7 +104,7 @@ J.camera.prototype.zoom = function(x, y, delta) {
   var future_zoom_level = Math.round((this._view[0] + wheel_sign * this._linear_zoom_factor)*10)/10;
 
   // clamp the linear pixel zoom
-  if (future_zoom_level < 1.0 || future_zoom_level >= 5.0) return;
+  if (future_zoom_level < 0.8 || future_zoom_level >= 5.0) return;
 
   if (future_w >= 0 && future_w < this._viewer._image.zoomlevel_count) {
     // start loading the tiles immediately but set no_draw to true
