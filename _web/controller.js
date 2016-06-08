6e79bde803dac09f5f219a976d17955e497d146f
thejohnhoffer <hoffexi@gmail.com>
2016-06-08 11:55:25 -0400

    Made flattened gl merge table actually temporary to allow correct undo/redo functionality.

--- _web/controller.js
+++ _web/controller.js
@@ -1302,7 +1302,7 @@ J.controller.prototype.merge = function(id) {
 
 J.controller.prototype.create_gl_merge_table = function() {
 
-  var mt = this._new_merge_table;
+  var mt = $.extend({},this._new_merge_table)
   var keys = Object.keys(mt);
   var no_keys = keys.length;
 
