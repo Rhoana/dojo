8b3aeb685ef8e46b935d8971f674682c65a5f3b0
thejohnhoffer <hoffexi@gmail.com>
2016-06-28 17:34:43 -0400

    fro pngs

--- _dojo/scripts/convert_png.py
+++ _dojo/scripts/convert_png.py
@@ -2,42 +2,112 @@
 
 from _dojo.scripts.np2imgo import Imgo
 from _dojo.scripts.np2sego import Sego
-from os import getcwd
+import matplotlib.pyplot as plt
+import numpy as np
 import glob
-import h5py
+import cv2
 import re
+import os
 
-in_folder = '../../../in'
+in_folder = '../../../derp'
 out_folder = '../../../out'
+img_path = '/em_padded'
+seg_path = '/np'
+type = '/*.png'
 
-find = lambda r,x: (x[i] for i in range(len(x)) if re.search(r,x[i]))
+blockshape = [2048,2048,40]
+nothing = np.uint8(0)
+shown = False
+space = '_'
+bit = 256
+
+def hash(p):
+    nums = [int(n) for n in p.split(space) if unicode(n).isnumeric()]
+    if len(nums) == 4:
+        nums = (nums[1:3]+nums[:1])*np.array(blockshape) + [0,0,nums[-1]]
+    elif len(nums) == 3:
+        nums = nums[1:3]+nums[:1]
+    return nums
+
+def grid(places):
+    plt.axes()
+    for row in places: plt.gca().add_patch(plt.Rectangle(row[1::-1], 1, 1, fc='k'))
+    ax = plt.axis('scaled')
+    plt.axis(ax[:2]+ax[:-3:-1])
+    plt.show()
+
 
 def convert(in_folder, out_folder):
-    files = glob.glob( in_folder + '/*.h5')
-    if not files : return 'no h5 found'
-    for fil in files:
-        f = h5py.File(fil, 'r')
 
-        # if labels in file
-        groups = f.keys()
-        seg = next(find(r'label',groups),-1)
-        if seg < 0 : return 'no labels found'
-        shape = f[seg].shape
-        groups.remove(seg)
+    places = []
+    folders = []
+    ids = np.zeros([bit]*3,dtype=np.uint8)
+    new_id = np.uint8(nothing+2)
 
-        # if image has same size
-        images = list(find(r'image',groups))
-        if not images: return 'no images found'
-        img = next((i for i in images if f[i].shape == shape),-1)
-        if img < 0: return 'label shape not same as image shape'
+    ## Z__Y__X to Y__X__Z
+    for dirt in os.listdir(in_folder):
+        dirt_path = os.path.join(in_folder, dirt)
+        if os.path.isdir(dirt_path):
+            places.append(hash(dirt))
+            folders.append(dirt_path)
+    places = np.array(places)
+    if shown: grid(places)
 
-        # Do labels
-        Sego(f[seg], out_folder)
+    ## Make emtpty arrays
+    layout = np.amax(places,0)+1
+    all_shape = layout*blockshape
+    img,seg = [np.zeros(all_shape)]*2
+    edge = blockshape[:-1] + [1,]
 
-        # Do image
-        Imgo(f[img], out_folder)
+    ## Fill arrays
+    for fold in folders:
+        seg_dirt = glob.glob(fold + seg_path)
+        img_dirt = glob.glob(fold + img_path)
+        if not seg_dirt: return 'segs not found'
+        if not img_dirt: return 'imgs not found'
 
-        f.close()
+
+        ## Use each file in both subfolders of all folders
+        for image in glob.glob(img_dirt[0] + type):
+            spot = hash(os.path.basename(image))
+            print  os.path.basename(image)
+            print  spot
+            print  '\n'
+            if len(spot) != 3 or (np.clip(spot,0,all_shape-edge)-spot).any(): return 'imgs not in bounds'
+            seg[spot[0]:(spot + edge)[0],spot[1]:(spot + edge)[1],spot[2]] = cv2.imread(image,0).T
+
+        ## Use each file in both subfolders of all folderss
+        for segment in glob.glob(seg_dirt[0] + type):
+            spot = hash(os.path.basename(segment))
+            print  os.path.basename(segment)
+            print  spot
+            print  '\n'
+            if len(spot) != 3 or (np.clip(spot,0,all_shape-edge)-spot).any(): return 'segs not in bounds'
+            # Read the image
+            out_seg = []
+            raw_seg = cv2.imread(segment).reshape(-1,3)
+            if not raw_seg.any():
+                result = np.ones(blockshape[:-1])
+            else:
+                for pix in raw_seg:
+                    out_seg.append(ids[tuple(pix)])
+                    if out_seg[-1] == np.uint8(0):
+                        ids[tuple(pix)] = new_id
+                        out_seg[-1] = new_id
+                        new_id += 1
+                result = np.array(out_seg).reshape(blockshape[:-1])
+
+            ## Fill in the image
+            img[spot[0]:(spot + edge)[0],spot[1]:(spot + edge)[1],spot[2]] = result
+
+
+
+    # Do labels
+    Sego(seg[:,:,2:], out_folder)
+
+    # Do image
+    Imgo(img[:,:,2:], out_folder)
+
 
 err = convert(in_folder, out_folder)
 if err: print 'err '+str(err)
